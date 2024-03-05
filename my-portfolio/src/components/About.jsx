15ecdcfde20b0efcedcb645485f58ac864f0296f
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-03-05 15:11:06 +0000

    "font"

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -18,7 +18,7 @@ const About = () => {
               <p>Hi. I'm Mohammed Tawfiq Iddrisu, nice to meet you. Please take a look around.</p>
             </div>
             <div>
-              <p>I am passionate about building excellent software that improves
+              <p className="font-anta">I am passionate about building excellent software that improves
               the lives of those around me. I specialize in creating software
               for clients ranging from individuals and small-businesses all the
               way to large enterprise corporations. What would you do if you had
