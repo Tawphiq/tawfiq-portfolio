9c96cdd9039751b5c3d05236e27b686437cd4d22
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-03-05 22:25:05 +0000

    "font style"

--- my-portfolio/src/components/Skills.jsx
+++ my-portfolio/src/components/Skills.jsx
@@ -13,7 +13,7 @@ import Python from '../assets/python-logo.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300 font-anta'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
           <div>
