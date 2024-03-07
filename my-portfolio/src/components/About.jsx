5adf267fc410fdf023c32fa2c316878ae4418055
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-03-07 02:13:00 +0000

    "about"

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -52,7 +52,7 @@ const About = () => {
 
 
   return (
-    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300 md:-mt-20 -mt-40'>
+    <div id='about' className='w-full h-screen bg-[#0a192f] text-gray-300 md:-mt-20 -mt-40'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-rightf pb-8 pl-4'>
