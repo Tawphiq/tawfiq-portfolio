66c13255c772ac44642f556ad3fde2801550520e
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-03-07 00:18:00 +0000

    "enhancements"

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -111,7 +111,8 @@ useEffect(() => {
           <li className='w-[160px] h-[60px] flex justify-between items-center mr-[-100px] md:ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://www.linkedin.com/in/mohammed-tawfiq-iddrisu-6469171b9/'
+              target='_blank'
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -119,7 +120,8 @@ useEffect(() => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://github.com/Tawphiq'
+              target='_blank'
             >
               Github <FaGithub size={30} />
             </a>
@@ -127,7 +129,8 @@ useEffect(() => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              href='https://mail.google.com/mail/u/?authuser=mohammedtawfiqiddrisu5@gmail.com'
+              target='_blank'
             >
               Email <HiOutlineMail size={30} />
             </a>
--- my-portfolio/src/components/Work.jsx
+++ my-portfolio/src/components/Work.jsx
@@ -8,7 +8,7 @@ const Work = () => {
     //setProject(data);
 
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f] pt-40'>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f] pt-40 pb-40'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
@@ -28,6 +28,7 @@ const Work = () => {
     className="shadow-lg shadow-[#040c16] group container rounded-md
               flex justify-center text-center items-center mx-auto content-div"
   >
+    
     {/* Hover effect for images */}
     <div className="opacity-0 group-hover:opacity-100 ">
       <span className="text-2xl font bold text-white tracking-wider ">
