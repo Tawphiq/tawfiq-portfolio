1061f79ef2644c0d968d11c360b2f249ef8ebf75
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-03-07 01:45:45 +0000

    "animations"

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -1,18 +1,41 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/no-unescaped-entities */
+import { useState, useEffect } from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 import { Link } from 'react-scroll';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
+  useEffect(() => {AOS.init({duration: 2000})}, [])
+  const [typedText, setTypedText] = useState('');
+
+  const originalText = `I'm a Full Stack Developer.`;
+
+  useEffect(() => {
+    let index = 0;
+    const typingInterval = setInterval(() => {
+      setTypedText(originalText.slice(0, index));
+      index += 1;
+
+      if (index > originalText.length) {
+        clearInterval(typingInterval);
+      }
+    }, 100);
+
+    return () => clearInterval(typingInterval);
+  }, []);
+
   return (
     <div name='home' className='w-full h-screen bg-[#0a192f]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-4 flex flex-col justify-center h-full'>
         <p className='text-pink-600 font-anta text-xl'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-5xl font-bold font-anta text-[#ccd6f6]'>
+        <h1 data-aos="fade-left"  className='text-4xl sm:text-5xl font-bold font-anta text-[#ccd6f6]'>
           MOHAMMED TAWFIQ IDDRISU
         </h1>
         <h2 className='text-4xl sm:text-5xl font-bold font-anta text-[#8892b0]'>
-          I'm a Full Stack Developer.
+          {typedText}
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px] font-anta'>
         I am a passionate tech enthusiast and a Full Stack Software Engineer with a strong foundation in various programming languages and web development frameworks.
@@ -20,7 +43,7 @@ const Home = () => {
 
         </p>
         <div>
-          <Link to='work' smooth={true} duration={500} className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 w-40 cursor-pointer'>
+          <Link to='work' smooth={true} duration={500} data-aos="fade-right" className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 w-40 cursor-pointer'>
             My Works
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
