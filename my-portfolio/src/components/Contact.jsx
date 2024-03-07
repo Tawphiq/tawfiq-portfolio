7750ce8d9e52c13741fe18acc87a93596fb9e43d
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-03-07 01:12:55 +0000

    "padding"

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -3,7 +3,7 @@
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center'>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center px-4 items-center'>
         <form method='POST' action="https://getform.io/f/lakookxa" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 font-anta'>Contact</p>
