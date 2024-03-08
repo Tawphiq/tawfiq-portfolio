a758d58a82ccf85a885764bdc4476cd4ba9c9d27
Tawphiq <mohammedtawfiqiddrisu5@gmail.com>
2024-03-08 01:19:23 +0000

    "socials"

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -108,7 +108,7 @@ useEffect(() => {
       {/* Social icons */}
       <div className='flex fixed flex-col top-[35%] left-0'>
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center mr-[-100px] md:ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+          <li className='w-[160px] h-[60px] flex justify-between items-center md:mr-[-100px] md:ml-[-100px] md:hover:ml-[-10px] duration-300 bg-blue-600'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
               href='https://www.linkedin.com/in/mohammed-tawfiq-iddrisu-6469171b9/'
